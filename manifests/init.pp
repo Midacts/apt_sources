a26f201c6fc8b6aa4d2213cd1e13fb5ddd597374
root <root@puppet.midacts.local>
2014-03-23 16:51:01 -0400

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,38 @@
+# == Class
+#
+# apt_sources
+#
+# == Synopsis
+#
+# This class is used for managing Debian the sources.list file with Puppet.
+#
+# == Author
+#
+# John McCarthy <midactsmystery@gmail.com>
+#
+# - http://www.midactstech.blogspot.com -
+# - https://www.github.com/Midacts -
+#
+# == Date
+#
+# 7th of March, 2014
+#
+# -- Version 1.0 --
+#
+class apt_sources {
+
+  file { '/etc/apt/sources.list':
+    ensure	=> present,
+    content	=> template('apt_sources/sources.list.erb'),
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
+  }
+    
+  exec { 'apt-get update':
+    command	=> '/usr/bin/apt-get update',
+    subscribe	=> File['/etc/apt/sources.list'],
+    refreshonly	=> true,
+  }
+
+}
